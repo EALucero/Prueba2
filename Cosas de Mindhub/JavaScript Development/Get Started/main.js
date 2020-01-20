f143b48bd6690f43656f7eed4f49945d92df1033
EALucero <56940163+EALucero@users.noreply.github.com>
2020-01-20 17:41:15 -0300

    Add files via upload

---
+++ Cosas de Mindhub/JavaScript Development/Get Started/main.js
@@ -0,0 +1,11 @@
+names = ["Agustín", "Alan", "Ariel", "Branko", "Diego", "Doris", "Eduardo", "Elías", "Érica", "Ezequiel", "Fresco", "Gabriel", "Gian", "Iván", "José", "Leandro", "Lucas1", "Lucas2", "Lucio", "Matías", "Meji", "Nahuel1", "Nahuel2", "Nicolás", "Rocío", "Rodrigo", "Román", "Teo"];
+
+var init = names[names.length/28];
+console.log(init);
+
+var last = names[names.length-1];
+console.log(last);
+
+for (i = 0; i < 28; i++) {
+  console.log("+i+""+names[i]+");
+}
