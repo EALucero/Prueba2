e8241b02f6334731a51745665ae3d7b4b040ae92
EALucero <56940163+EALucero@users.noreply.github.com>
2020-01-21 17:48:05 -0300

    Add files via upload

--- Cosas de Mindhub/JavaScript Development/Get Started/JavaScript Basics/main.js
+++ Cosas de Mindhub/JavaScript Development/Get Started/JavaScript Basics/main.js
@@ -1,8 +1,14 @@
+console.log("+++ Ej.1 +++");
+
 var myName = ["Eduardo"];
 console.log(myName);
 
+console.log("+++ Ej.2 +++");
+
 var age = [25];
 console.log(age);
+
+console.log("+++ Ej.3 +++");
 
 var ignasiAge = [32];
 console.log(ignasiAge);
@@ -10,11 +16,15 @@ console.log(ignasiAge);
 ageDiff = ignasiAge - age;
 console.log(ageDiff);
 
+console.log("+++ Ej.4 +++");
+
 if (age>21) {
   console.log("You are older than 21");
 }else {
   console.log("You are not older than 21");
 }
+
+console.log("+++ Ej.5 +++");
 
 if (age<ignasiAge) {
   console.log("Ignasi is older than you");
--- Cosas de Mindhub/JavaScript Development/Get Started/main.js
+++ Cosas de Mindhub/JavaScript Development/Get Started/main.js
@@ -1,11 +1,57 @@
-names = ["Agustín", "Alan", "Ariel", "Branko", "Diego", "Doris", "Eduardo", "Elías", "Érica", "Ezequiel", "Fresco", "Gabriel", "Gian", "Iván", "José", "Leandro", "Lucas1", "Lucas2", "Lucio", "Matías", "Meji", "Nahuel1", "Nahuel2", "Nicolás", "Rocío", "Rodrigo", "Román", "Teo"];
+console.log("+++ Ej.1 +++");
 
-var init = names[names.length/28];
-console.log(init);
+names = ["Iván", "José", "Gian", "Leandro", "Gabriel", "Lucas1", "Fresco", "Lucas2", "Ezequiel", "Lucio", "Erica", "Matías", "Elías", "Meji", "Eduardo", "Nahuel1", "Doris", "Nahuel2", "Diego", "Nicolás", "Branko", "Rocío", "Ariel", "Rodrigo", "Alan", "Román", "Agustín", "Teo"];
+
+names[names.length];
+console.log(names[0]);
 
 var last = names[names.length-1];
 console.log(last);
 
-for (i = 0; i < 28; i++) {
-  console.log("+i+""+names[i]+");
+names.sort()
+
+for (i = 0; i < names.length; i++) {
+  console.log(names[i]);
 }
+
+console.log("+++ Ej.2 +++");
+
+ages = ["17", "23", "20", "21", "24", "23", "21", "18", "20", "19", "17", "19", "20", "32", "23", "24", "17", "26", "18", "28", "21", "25", "18", "27", "19", "21", "19", "18"];
+
+console.log("-- edades while --");
+
+i = 0;
+
+while (i < ages.length) {
+  console.log(ages[i]);
+  i++;
+}
+
+console.log("-- pares --");
+
+var events = [0];
+
+while (i < ages.length) {
+
+
+  if (ages[i] % 2 === 0) {
+    console.log(events);
+    i++;
+  }
+}
+
+console.log("-- edades for --");
+
+for (i = 0; i < ages.length; i++) {
+  console.log(ages[i]);
+}
+
+console.log("+++ Ej.3 +++");
+
+console.log(Math.min.apply(null, ages));
+
+console.log("+++ Ej.4 +++");
+
+console.log(Math.max.apply(null, ages));
+
+console.log("+++ Ej.5 +++");
