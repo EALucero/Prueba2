e6398501b47106bb83db8186e42a9c78d3e38510
EALucero <56940163+EALucero@users.noreply.github.com>
2020-02-03 17:50:11 -0300

    Add files via upload

---
+++ Cosas de Mindhub/JavaScript Development/Access and Display Public Data/main.js
@@ -0,0 +1,70 @@
+const members = data.results[0].members
+
+const tbody = document.querySelector("tbody")
+
+function checkbox(){
+  document.getElementById("table-data").innerHTML = ""
+  let state = document.getElementById("inputState").value
+  let party = document.getElementsByClassName("congress")
+  let select = []
+  let dFilter = []
+
+//party filter
+    for (let i = 0; i <party.length; i++) {
+      if (party[i].checked) {
+        dFilter = members.filter(e => e.party == party[i].value)
+
+        members.filter(e => e.party == party[i].value && (e.state == state || state == "all")). forEach(member => {
+        let row = tbody.insertRow(-1);
+
+          if (member.middle_name == null) {
+            if (member.url == "") {
+              row.innerHTML = `<td>${member.first_name} ${member.last_name}</td>
+              <td>${member.party}</td>
+              <td>${member.state}</td>
+              <td>${member.seniority}</td>
+              <td>${member.votes_with_party_pct+'%'}</td>`
+            }else {
+              row.innerHTML = `<td><a href=${member.url}>${member.first_name} ${member.last_name}</td>
+              <td>${member.party}</td>
+              <td>${member.state}</td>
+              <td>${member.seniority}</td>
+              <td>${member.votes_with_party_pct+'%'}</td>`
+            }
+          }else {
+            if (member.url == "") {
+              row.innerHTML = `<td>${member.first_name} ${member.middle_name} ${member.last_name}</td>
+              <td>${member.party}</td>
+              <td>${member.state}</td>
+              <td>${member.seniority}</td>
+              <td>${member.votes_with_party_pct+'%'}</td>`
+            }else {
+              row.innerHTML = `<td><a href=${member.url}>${member.first_name} ${member.middle_name} ${member.last_name}</td>
+              <td>${member.party}</td>
+              <td>${member.state}</td>
+              <td>${member.seniority}</td>
+              <td>${member.votes_with_party_pct+'%'}</td>`
+            }
+          }
+        }); //se que se puede hacer con ternarios pero tampoco es mucho ahorro de codigín.
+      }
+
+//state filter
+      dFilter.forEach (e => {
+        if (select.indexOf(e.state)== (-1)) {
+          select.push(e.state)
+        }
+      });
+
+      document.getElementById("inputState").innerHTML = `<option value = "all">all</option>`
+      select.forEach (e => {document.getElementById("inputState").innerHTML += `<option value ="${e}">${e}</option>`});
+      document.getElementById("inputState").value = state
+    }
+  }
+
+//checkbox
+    document.getElementById("dem").addEventListener("click", checkbox)
+    document.getElementById("rep").addEventListener("click", checkbox)
+    document.getElementById("ind").addEventListener("click", checkbox)
+    checkbox()
+    document.getElementById("inputState").addEventListener("change", checkbox)
---
+++ Cosas de Mindhub/JavaScript Development/Calculate and Display Statistics/js/stats.js
@@ -0,0 +1,15 @@
+let stats = {
+  n_dem = 0
+  n_rep = 0
+  n_ind = 0
+
+  least_loyal = []
+  most_loyal = []
+}
+
+
+
+
+
+
+//hacer mañana!!!
