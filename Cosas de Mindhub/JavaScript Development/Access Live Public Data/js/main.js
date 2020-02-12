eb287b5e87173fe1a5f083898603a8f2f217f4c9
EALucero <56940163+EALucero@users.noreply.github.com>
2020-02-12 17:53:48 -0300

    Add files via upload

--- Cosas de Mindhub/JavaScript Development/Access Live Public Data/js/main.js
+++ Cosas de Mindhub/JavaScript Development/Access Live Public Data/js/main.js
@@ -1,48 +1,50 @@
-let table = new Vue ({
+const table = new Vue ({
   el: '#table',
   data: {
+    senBers : "https://api.propublica.org/congress/v1/113/senate/members.json",
+    houBers : "https://api.propublica.org/congress/v1/113/house/members.json",
+    init : {
+      method:'GET',
+      headers:{'X-API-Key':'UWw13xqhQGs8N72PqRckDFIE8gXG6xLLSJ54MUtm'}
+    },
+    filtLiar: {states : [], select : document.getElementById("states"), members : data.result[0].members},
     stats : {nDem : 0, nRep : 0, nInd : 0, vDem : 0, vRep : 0, vInd : 0,
         tpDem : [], tpRep : [], tpInd : [], tpTotal : [],
         total : data.results[0].num_results,
         lLoyal : [], mLoyal : [], lAtten : [], mAtten : []}
-      }
-  });
+    },
 
-let data
-let senBers = "https://api.propublica.org/congress/v1/113/senate/members.json"
-let houBers = "https://api.propublica.org/congress/v1/113/house/members.json"
-let init = {
-  method:'GET',
-  headers:{'X-API-Key':'UWw13xqhQGs8N72PqRckDFIE8gXG6xLLSJ54MUtm'}
-}
+  created(){
+        fetch(data.senBers, data.init)
+          .then(function(res){
+            if(res.ok){
+              return res.json()
+            } else{
+              throw new Error(res.status)
+            }
+          })
+          .then(function(senate){
+            data = json
+            gallery(data)
+          })
+          .catch(function(error){
+            console.log(error)
+          })
 
-fetch(senBers, init)
-  .then(function(res){
-    if(res.ok){
-      return res.json()
-    } else{
-      throw new Error(res.status)
-    }
-  })
-  .then(function(senate){
-    data = json
-    gallery(data)
-  })
-  .catch(function(error){
-    console.log(error)
-  })
+          console.log("hola")
 
-  console.log("hola")
+          function gallery(json){
+            json.forEach(e => {
+              let img = new Image()
+              img.src = e.thumbnailUrl
 
-  function gallery(json){
-    json.forEach(e => {
-      let img = new Image()
-      img.src = e.thumbnailUrl
+              let div = document.createElement('DIV')
 
-      let div = document.createElement('DIV')
+              div.appendChild(img)
 
-      div.appendChild(img)
+              document.getElementById("tbody").appendChild(div)
+            })
+          }
+        }
 
-      document.getElementById("tbody").appendChild(div)
-    })
-  }
+  })  
